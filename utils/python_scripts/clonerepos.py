7b073c5dbceb276b36735fabd5845a254be1fddc
Zubair <zubairtusar17@gmail.com>
2021-07-06 20:02:53 +0600

    clone repos using github api

--- utils/python_scripts/clonerepos.py
+++ utils/python_scripts/clonerepos.py
@@ -1,18 +1,34 @@
-import csv
-
-#import git
+import requests
 from git import Repo
+import argparse
+import pathlib
 
-index = 0
+response = requests.get("https://api.github.com/search/repositories?q=language:c&sort=stars")
 
-file = open("G:\Personal\Edu\Academics\Research Works\dataset\datasetPython1000.csv", "r")
-reader = csv.reader(file, delimiter=',')
-for column in reader:
-    index = index + 1
-    if(index > 2):
-        print(column[0])
-        repository = "https://github.com/"+column[0].split()[0]+".git"
-        print(repository)
-        #git.Git("G:\Personal\Edu\Academics\Research Works\Repos").clone("git@github.com:"+column[0]+".git")    #method 1; either do this
-        Repo.clone_from("https://github.com/"+column[0]+".git", "G:\Personal\Edu\Academics\Research Works\Repos\\"+str(index))   #method 2; or do this
-        print("cloned")
+print("status code: ", response.status_code)
+
+res = response.json()
+data = res["items"]
+
+i = 0
+
+for data_item in data:
+    print(data_item["html_url"])
+    repository = data_item["html_url"]+".git"
+    print(repository)
+    #git.Git("G:\Personal\Edu\Academics\Research Works\Repos").clone("git@github.com:"+column[0]+".git")    #method 1; either do this
+    Repo.clone_from(repository, "G:\Personal\Edu\Academics\Research Works\Repos\\" + data_item["html_url"].split('/')[4])   #method 2; or do this
+    print("cloned")
+
+path = "G:\Personal\Edu\Academics\Research Works\Repos"
+
+pathlist = pathlib.Path(path).glob('**/*.c')
+
+for path in pathlist:
+     # because path is object not string
+     path_in_str = str(path)
+     print(path_in_str)
+     
+
+
+
